8436cbd03c1291df2519298d93641309cd8a34c5
Emmanuelhfc <emmanuelhenriquefc@gmail.com>
2024-11-03 15:11:48 -0300

    [remove] 🗑️: remove old files

--- dataBase/connection.py
+++
@@ -1,4 +0,0 @@
-import sqlalchemy
-
-def engine():
-    return sqlalchemy.create_engine("sqlite:///dataBase/db.sqlite3", echo=True)
--- dataBase/constants.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-DATA_BASE_DIR = Path(__file__).parent
-DB_CONSTANTS_DIR = DATA_BASE_DIR / "casco_tubo_constants.sqlite"
-
