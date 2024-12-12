a8737af93730230a1c42a58e84f479708fcf013a
carvalhoe <carvalhoe@weg.net>
2024-12-12 13:39:36 -0300

    [feat] ✨: calc nsga2 pela interface com id inp existente

--- djangoapp/frontend/views/nsga2_inputs.py
+++ djangoapp/frontend/views/nsga2_inputs.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from frontend.forms import*
+from django.urls import reverse
+from django.http import HttpResponseBadRequest
+from frontend.utils import*
+import requests
+import logging
 
+logger = logging.getLogger('frontend')
 
 def nsga2_inputs(request):
     sthe_form = STHEForm(request.POST or None)
     filter_input_form = FilterInputNSGA2Form(request.POST or None)
+    endpoint_nsga2 = api_endpoint(request, reverse('API:nsga2_sthe_optimization'))
+
+    if filter_input_form.is_valid():
+        data = filter_input_form.cleaned_data
+        resp_nsga2 = requests.post(endpoint_nsga2, data=data)
+        
+        if resp_nsga2.status_code != 200:
+            return HttpResponseBadRequest()
+        
+        calculation_id = resp_nsga2.json()[0]['calculation_id']
+
+        return redirect(reverse('frontend:pareto_front', kwargs={"calculation_id": calculation_id}))
+
+
+
 
     context = {
         'form_title': "Avaliação",
