b3b1e3aa1ba6eff54b9497c8fc341de9f78c61a5
Emmanuelhfc <emmanuelhenriquefc@gmail.com>
2025-02-18 19:33:10 -0300

    [feat] ✨: configure nginx

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -131,6 +131,12 @@ else:
             "NAME": "db.sqlite3",
         }
     }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
+    }
+}
 
 
 
--- djangoapp/scripts/commands.sh
+++ djangoapp/scripts/commands.sh
@@ -10,10 +10,10 @@ python manage.py migrate --noinput
 
 if [ "$PRODUCTION" = "1" ]; then
     echo 'Gunicorn'
-    gunicorn project.wsgi:application --bind 0.0.0.0:8100 --workers 1 --threads 8 --timeout 0
+    gunicorn project.wsgi:application --bind 0.0.0.0:8000 --workers 3 
 else
     echo 'Run Server'
-    python manage.py runserver 0.0.0.0:80
+    python manage.py runserver 0.0.0.0:8000
 fi
 
 # python manage.py runserver 0.0.0.0:8100PORT --workers 1 --threads 8 --timeout 0 tutorial.wsgi:application
