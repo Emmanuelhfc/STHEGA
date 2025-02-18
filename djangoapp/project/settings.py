9cc62a497416b5b98d2ca424532c29b89c253444
Emmanuelhfc <emmanuelhenriquefc@gmail.com>
2025-02-18 11:58:09 -0300

    [feat] ✨: update settings.py

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -33,21 +33,21 @@ env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, ".env")
 
 
-if os.path.isfile(env_file):
-    # Use a local secret file, if provided
-    env.read_env(env_file)
+# if os.path.isfile(env_file):
+#     # Use a local secret file, if provided
+#     env.read_env(env_file)
 
-elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-    client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+# elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+#     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+#     client = secretmanager.SecretManagerServiceClient()
+#     settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+#     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+#     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
-    env.read_env(io.StringIO(payload))
+#     env.read_env(io.StringIO(payload))
 
-else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+# else:
+#     raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 
 ALLOWED_HOSTS = ['localhost']
 ALLOWED_HOSTS.extend(os.getenv('ALLOWED_HOSTS', '').split(','))
@@ -234,7 +234,7 @@ LOGGING = {
     },
 }
 
-GS_BUCKET_NAME = env("GS_BUCKET_NAME")
-GS_DEFAULT_ACL = "publicRead"
-DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
-STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+# GS_BUCKET_NAME = env("GS_BUCKET_NAME")
+# GS_DEFAULT_ACL = "publicRead"
+# DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+# STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
